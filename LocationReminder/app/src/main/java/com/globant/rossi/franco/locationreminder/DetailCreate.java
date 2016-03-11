9b589d44aef46417093b0d58c141903628096f33
Franco Rossi <franco.rossi@wnco.com>
2016-03-11 17:56:43 -0300

    Final Project - Added support to Google Places

--- LocationReminder/app/src/main/java/com/globant/rossi/franco/locationreminder/DetailCreate.java
+++ LocationReminder/app/src/main/java/com/globant/rossi/franco/locationreminder/DetailCreate.java
@@ -1,13 +1,75 @@
 package com.globant.rossi.franco.locationreminder;
 
+import android.content.res.ColorStateList;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Places;
 
 public class DetailCreate extends AppCompatActivity {
+//    ColorStateList locationIconColor;
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_create);
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        EditText locationET = (EditText) findViewById(R.id.location);
+//        locationIconColor = locationET.getCompoundDrawableTintList();
+        locationET.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                locationFocused(v, hasFocus);
+            }
+        });
+        locationET.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLocation(v);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+
+    private void locationFocused(View v, boolean hasFocus) {
+        if (hasFocus) {
+            getLocation(v);
+//            ((EditText) v).setCompoundDrawableTintList(ColorStateList.valueOf(getColor(R.color.colorAccent)));
+        }
+/*
+        else{
+            ((EditText) v).setCompoundDrawableTintList(locationIconColor);
+        }
+*/
+    }
+
+    public void getLocation(View v) {
+//        Toast.makeText(DetailCreate.this, "Get Location", Toast.LENGTH_SHORT).show();
+
     }
 }
--- LocationReminder/app/src/main/java/com/globant/rossi/franco/locationreminder/MainActivity.java
+++ LocationReminder/app/src/main/java/com/globant/rossi/franco/locationreminder/MainActivity.java
@@ -1,9 +1,11 @@
 package com.globant.rossi.franco.locationreminder;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton addReminderFAB = (FloatingActionButton) findViewById(R.id.add_fab);
+        addReminderFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addReminder(v);
+            }
+        });
     }
 
-    public void addReminder(View v){
+    public void addReminder(View v) {
         Intent intent = new Intent(this, DetailCreate.class);
         startActivity(intent);
     }
