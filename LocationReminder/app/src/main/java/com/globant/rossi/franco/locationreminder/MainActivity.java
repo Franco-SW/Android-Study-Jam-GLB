74b4be638ef04ab17e73fdb9e9b66bc4a75cf9ad
Franco Rossi <franco.rossi@wnco.com>
2016-03-11 13:45:26 -0300

    Final Project - Modifications to Activities and Styles

--- LocationReminder/app/src/main/java/com/globant/rossi/franco/locationreminder/MainActivity.java
+++ LocationReminder/app/src/main/java/com/globant/rossi/franco/locationreminder/MainActivity.java
@@ -1,7 +1,9 @@
 package com.globant.rossi.franco.locationreminder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addReminder(View v){
+        Intent intent = new Intent(this, DetailCreate.class);
+        startActivity(intent);
     }
 }
